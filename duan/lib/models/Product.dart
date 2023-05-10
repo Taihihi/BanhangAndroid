fbdffe00a5ee5c27216af07be185f0b43728d551
nop bai <taihuu113456@gmail.com>
2023-05-10 21:29:47 +0700

    sua anh

--- duan/lib/models/Product.dart
+++ duan/lib/models/Product.dart
@@ -15,24 +15,24 @@ class Product {
 
 List<Product> demo_product = [
   Product(
-    image: "assets/images/product_0.png",
+    image: "assets/images/product_01.png",
     title: "Long Sleeve Shirts",
     price: 165,
     bgColor: const Color(0xFFFEFBF9),
   ),
   Product(
-    image: "assets/images/product_1.png",
+    image: "assets/images/product_02.png",
     title: "Casual Henley Shirts",
     price: 99,
   ),
   Product(
-    image: "assets/images/product_2.png",
+    image: "assets/images/product_03.png",
     title: "Curved Hem Shirts",
     price: 180,
     bgColor: const Color(0xFFF8FEFB),
   ),
   Product(
-    image: "assets/images/product_3.png",
+    image: "assets/images/product_04.png",
     title: "Casual Nolin",
     price: 149,
     bgColor: const Color(0xFFEEEEED),
